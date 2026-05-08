64f5d827cc732a4341e597532bf40ad6dd064623
walon <walon@code-philosophy.com>
2026-05-08 22:14:10 +0800

    chore: `aot-runner/run.bat` forgot rename simple-aot to aot-runner after renaming project

--- src/leanaot/aot-runner/run.bat
+++ src/leanaot/aot-runner/run.bat
@@ -3,4 +3,4 @@ call build.bat
 
 call dotnet build ..\..\leanaot\Test\Test.csproj
 
-build\bin\Debug\simple-aot.exe -l ..\..\libraries\dotnetframework4.x -l ..\..\leanaot\Test\bin\Debug -e App::Main Test
+build\bin\Debug\aot-runner.exe -l ..\..\libraries\dotnetframework4.x -l ..\..\leanaot\Test\bin\Debug -e App::Main Test
