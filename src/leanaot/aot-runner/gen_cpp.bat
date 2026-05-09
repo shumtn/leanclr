458d0e8b1c56847562737cb5d66cc82b831bd213
walon <walon@code-philosophy.com>
2026-05-09 23:23:24 +0800

    chore: gen_cpp.bat and gen_cpp_wasm.bat dont' call build LeanAOT

--- src/leanaot/aot-runner/gen_cpp.bat
+++ src/leanaot/aot-runner/gen_cpp.bat
@@ -2,21 +2,14 @@
 setlocal
 cd /d "%~dp0"
 
-echo [1/3] Building leanaot Test project (Debug)...
+echo [1/2] Building leanaot Test project (Debug)...
 dotnet build "..\..\leanaot\Test\Test.csproj" -c Debug
 if errorlevel 1 (
 	echo Test build failed.
 	exit /b %ERRORLEVEL%
 )
 
-echo [2/3] Building LeanAOT (Debug)...
-dotnet build "..\LeanAOT\LeanAOT.csproj" -c Debug
-if errorlevel 1 (
-	echo LeanAOT build failed.
-	exit /b %ERRORLEVEL%
-)
-
-echo [3/3] Running LeanAOT...
+echo [2/2] Running LeanAOT...
 "..\LeanAOT\bin\Debug\net8.0\LeanAOT.exe" ^
   -o cpp ^
   -d ..\..\libraries\dotnetframework4.x ^
