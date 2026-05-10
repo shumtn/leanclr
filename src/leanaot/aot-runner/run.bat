95114c53b8d35ad63394540094e2cc5b49485c55
walon <walon@code-philosophy.com>
2026-05-10 15:02:24 +0800

    chore(aot-runner): comment building Test in run.bat

--- src/leanaot/aot-runner/run.bat
+++ src/leanaot/aot-runner/run.bat
@@ -1,6 +1,6 @@
 
 call build.bat
 
-call dotnet build ..\..\leanaot\Test\Test.csproj
+rem call dotnet build ..\..\leanaot\Test\Test.csproj
 
 build\bin\Debug\aot-runner.exe -l ..\..\libraries\dotnetframework4.x -l ..\..\leanaot\Test\bin\Debug -e App::Main Test
